6132b2cb6a828a5d5c97c596c990e19bb99f3119
tekgator <info@tekgator.com>
2020-01-01 18:57:49 +0100

    Add config.h file

---
+++ include/config.h
@@ -0,0 +1,12 @@
+#ifndef mysecrets_h
+#define mysecrets_h
+
+#define WIFI_SSID "SSID here"
+#define WIFI_PASSWORD "WPA key here"
+
+#define MQTT_HOST IPAddress(1, 1, 1, 1)
+#define MQTT_PORT 1883
+#define MQTT_USER "MQTT user here"
+#define MQTT_PWD "MQTT password here"
+
+#endif
