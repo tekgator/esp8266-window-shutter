fedba530bc67374c83c1a72fb44d0dc0c11d28a6
tekgator <info@tekgator.com>
2020-01-01 23:33:51 +0100

    add more config options

--- include/config.h
+++ include/config.h
@@ -1,12 +1,17 @@
-#ifndef mysecrets_h
-#define mysecrets_h
+#ifndef config_h
+#define config_h
+
+#define CLIENT_ID_PREFIX "esp8266"
 
 #define WIFI_SSID "SSID here"
 #define WIFI_PASSWORD "WPA key here"
 
-#define MQTT_HOST IPAddress(1, 1, 1, 1)
+#define MQTT_HOST IPAddress(192, 168, 1, 2)
 #define MQTT_PORT 1883
 #define MQTT_USER "MQTT user here"
 #define MQTT_PWD "MQTT password here"
+
+/* defines whether, after WiFi and MQTT is connected, the onboard LED stays active */
+#define LED_ONBOARD_ACTIVE false
 
 #endif
