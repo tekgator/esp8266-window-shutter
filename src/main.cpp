6555a0213909fb7ba0a31cff70ea6496e6400f83
Patrick Weiss <info@tekgator.com>
2021-03-06 01:38:30 +0100

    Add WiFiManager to avoid having static config

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,36 @@
+#include <FS.h>
+#include <LittleFS.h>
 #include <Arduino.h>
 #include <ArduinoLog.h>
-#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
-#include <Ticker.h>
+#include <WiFiManager.h>
+#include <ArduinoJson.h>
 #include <PubSubClient.h>
+#include <Ticker.h>
+
 #include "config.h"
 #include "Shutter.hpp"
 
-Shutter m_shutter1("left");
-Shutter m_shutter2("right");
+Shutter shutter1("left");
+Shutter shutter2("right");
 
-WiFiEventHandler m_wifiConnectHandler;
-WiFiEventHandler m_wifiDisconnectHandler;
-WiFiEventHandler m_wifiGotIpHandler;
-String m_clientId;
-WiFiClient m_wifiClient;
-Ticker m_wifiReconnectTimer;
+bool shouldSaveConfig = false;
+char mqttServer[40] = "";
+char mqttPort[6] = "1883";
+char mqttUser[40] = "";
+char mqttPassword[40] = "";
+char shutterDelay[5] = "1500";
 
-PubSubClient m_mqttClient(m_wifiClient);
-unsigned long m_mqttLastReconnectAttempt = 0;
+WiFiEventHandler wifiConnectHandler;
+WiFiEventHandler wifiDisconnectHandler;
+WiFiEventHandler wifiGotIpHandler;
+String clientId;
+WiFiClient wifiClient;
+
+PubSubClient mqttClient(wifiClient);
+unsigned long mqttLastReconnectAttempt = 0;
 
 enum MqttMode {
     INVALID_MQTT_MODE = -100,
@@ -28,21 +40,21 @@ enum MqttMode {
     SHUTTER2 = 2,
 };
 
-Ticker m_onboardLedBlinker;
+Ticker onboardLedBlinker;
 
 void blinkOnboardLed() {
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 }
 
 void startBlinkOnboardLed(bool critical = false) {
-    if (!m_onboardLedBlinker.active()) {
-        m_onboardLedBlinker.attach_ms(critical ? 500 : 1000, blinkOnboardLed);
+    if (!onboardLedBlinker.active()) {
+        onboardLedBlinker.attach_ms(critical ? 200 : 800, blinkOnboardLed);
     }
 }
 
 void stopBlinkOnboardLed() {
-    if (m_onboardLedBlinker.active()) {
-        m_onboardLedBlinker.detach();
+    if (onboardLedBlinker.active()) {
+        onboardLedBlinker.detach();
         digitalWrite(LED_BUILTIN, LED_ONBOARD_ACTIVE ? LOW : HIGH);
     }
 }
@@ -63,7 +75,7 @@ String buildMqttTopic (String subTopic, MqttMode mqttMode) {
     if (mqttMode == MqttMode::GLOBAL) {
         mqttTopic = String(CLIENT_ID_PREFIX) + "s/";
     } else {
-        mqttTopic = m_clientId + "/";
+        mqttTopic = clientId + "/";
     }
 
     if (mqttMode > MqttMode::DEVICE) {
@@ -76,22 +88,22 @@ String buildMqttTopic (String subTopic, MqttMode mqttMode) {
 
 void subscribeMqttTopic(String topic) {
     Log.notice("[ %s:%d ] Subcribe to MQTT topic [ %s ].", __FILE__, __LINE__, topic.c_str());
-    m_mqttClient.subscribe(topic.c_str());
+    mqttClient.subscribe(topic.c_str());
 }
 
 void publishMqttTopic(String topic, String payload) {
     Log.notice("[ %s:%d ] Publish MQTT topic [ %s ] with payload [ %s ].", __FILE__, __LINE__, topic.c_str(), payload.c_str());
-    m_mqttClient.publish(topic.c_str(), payload.c_str());
+    mqttClient.publish(topic.c_str(), payload.c_str());
 }
 
 void sendStatusShutter1Mqtt() {
-    publishMqttTopic(buildMqttTopic("state", MqttMode::SHUTTER1), m_shutter1.getStatus());
-    publishMqttTopic(buildMqttTopic("position", MqttMode::SHUTTER1), String(m_shutter1.getPosition()));
+    publishMqttTopic(buildMqttTopic("state", MqttMode::SHUTTER1), shutter1.getStatus());
+    publishMqttTopic(buildMqttTopic("position", MqttMode::SHUTTER1), String(shutter1.getPosition()));
 }
 
 void sendStatusShutter2Mqtt() {
-    publishMqttTopic(buildMqttTopic("state", MqttMode::SHUTTER2), m_shutter2.getStatus());
-    publishMqttTopic(buildMqttTopic("position", MqttMode::SHUTTER2), String(m_shutter2.getPosition()));
+    publishMqttTopic(buildMqttTopic("state", MqttMode::SHUTTER2), shutter2.getStatus());
+    publishMqttTopic(buildMqttTopic("position", MqttMode::SHUTTER2), String(shutter2.getPosition()));
 }
 
 void announceMqtt() {
@@ -191,9 +203,9 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
 
             if (isValid) {
                 if (mqttMode == MqttMode::SHUTTER1) {
-                    m_shutter1.executeAction(shutterAction, position, m_shutter2.isActionInProgress());
+                    shutter1.executeAction(shutterAction, position, shutter2.isActionInProgress());
                 } else if (mqttMode == MqttMode::SHUTTER2) {
-                    m_shutter2.executeAction(shutterAction, position, m_shutter1.isActionInProgress());
+                    shutter2.executeAction(shutterAction, position, shutter1.isActionInProgress());
                 }                
             }
         } else if (mqttMode == MqttMode::GLOBAL) {
@@ -212,13 +224,23 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
 
 bool connectToMqtt() {
     bool connected;
-    
-    Log.notice("[ %s:%d ] Connecting to MQTT broker [ %s:%d ] with client ID [ %s ].", __FILE__, __LINE__, MQTT_HOST.toString().c_str(), MQTT_PORT, m_clientId.c_str());
+
+    char *user = NULL;
+    char *pwd = NULL;
+
+    if (strlen(mqttUser) > 0) {
+        user = mqttUser;
+    }
+    if (strlen(mqttPassword) > 0) {
+        pwd = mqttPassword;
+    }
+
+    Log.notice("[ %s:%d ] Connecting to MQTT broker [ %s:%s ] with client ID [ %s ].", __FILE__, __LINE__, mqttServer, mqttPort, clientId.c_str());
    
-    connected = m_mqttClient.connect(m_clientId.c_str(), MQTT_USER, MQTT_PWD, buildMqttTopic("availability", MqttMode::DEVICE).c_str(), 0, true, "offline", true);
+    connected = mqttClient.connect(clientId.c_str(), user, pwd, buildMqttTopic("availability", MqttMode::DEVICE).c_str(), 0, true, "offline", true);
 
     if (connected) {
-        Log.notice("[ %s:%d ] Successfully connected to MQTT broker [ %s:%d ]", __FILE__, __LINE__, MQTT_HOST.toString().c_str(), MQTT_PORT);
+        Log.notice("[ %s:%d ] Successfully connected to MQTT broker [ %s:%d ]", __FILE__, __LINE__, mqttServer, mqttPort);
 
         subscribeMqttTopic(buildMqttTopic("command", MqttMode::GLOBAL));
 
@@ -230,32 +252,32 @@ bool connectToMqtt() {
 
         announceMqtt();
     } else {
-        Log.error("[ %s:%d ] Failed connection to MQTT broker [ %s:%d ] with status [ %d ].", __FILE__, __LINE__, MQTT_HOST.toString().c_str(), MQTT_PORT, m_mqttClient.state());
+        Log.error("[ %s:%d ] Failed connection to MQTT broker [ %s:%d ] with status [ %d ].", __FILE__, __LINE__, mqttServer, mqttPort, mqttClient.state());
     }
 
     return connected;
 }
 
 void setupMqtt() {
-    m_mqttClient.setServer(MQTT_HOST, MQTT_PORT);
-    m_mqttClient.setCallback(mqttCallback);
+    mqttClient.setServer(mqttServer, String(mqttPort).toInt());
+    mqttClient.setCallback(mqttCallback);
 }
 
 bool checkMqttConnection() {
     bool isConnected = false;
     
     if (WiFi.isConnected()) {
-        if (m_mqttClient.connected()) {
+        if (mqttClient.connected()) {
             stopBlinkOnboardLed();        
-            m_mqttClient.loop();
+            mqttClient.loop();
             isConnected = true;
         } else {
             startBlinkOnboardLed();
             ulong now = millis();
-            if (now - m_mqttLastReconnectAttempt > 2000) {
-                m_mqttLastReconnectAttempt = now;
+            if (now - mqttLastReconnectAttempt > 5000) {
+                mqttLastReconnectAttempt = now;
                 if (connectToMqtt()) {
-                    m_mqttLastReconnectAttempt = 0;
+                    mqttLastReconnectAttempt = 0;
                 }
             }
         }
@@ -269,7 +291,7 @@ void shutterActionInProgress(String id, ShutterAction shutterAction) {
 }
 
 void shutterActionComplete(String id, ShutterAction shutterAction, ShutterReason reason) {
-    if (m_shutter1.getID() = id) {
+    if (shutter1.getID() = id) {
         sendStatusShutter1Mqtt();
     } else {
         sendStatusShutter2Mqtt();
@@ -277,23 +299,30 @@ void shutterActionComplete(String id, ShutterAction shutterAction, ShutterReason
 }
 
 void setupShutter() {
-    Log.notice("[ %s:%d ] Setup shutter 1.", __FILE__, __LINE__);
-    m_shutter1.setControlPins(D5, D6, D7);
-    m_shutter1.setDurationFullMoveMs(15650);
-    m_shutter1.onActionInProgress(shutterActionInProgress);
-    m_shutter1.onActionComplete(shutterActionComplete);
+    Log.notice("[ %s:%d ] Setup shutter 1", __FILE__, __LINE__);
+    shutter1.setControlPins(D5, D6, D7);
+    shutter1.setDurationFullMoveMs(15650);
+    shutter1.onActionInProgress(shutterActionInProgress);
+    shutter1.onActionComplete(shutterActionComplete);
 
-    Log.notice("[ %s:%d ] Setup shutter 2.", __FILE__, __LINE__);
-    m_shutter2.setControlPins(D1, D2, D3);
-    m_shutter2.setDurationFullMoveMs(15000);
-    m_shutter2.onActionInProgress(shutterActionInProgress);
-    m_shutter2.onActionComplete(shutterActionComplete);
+    Log.notice("[ %s:%d ] Setup shutter 2", __FILE__, __LINE__);
+    shutter2.setControlPins(D1, D2, D3);
+    shutter2.setDurationFullMoveMs(15000);
+    shutter2.onActionInProgress(shutterActionInProgress);
+    shutter2.onActionComplete(shutterActionComplete);
 }
 
-void connectToWifi() {
-    Log.notice("[ %s:%d ] Connecting to SSID [ %s ].", __FILE__, __LINE__, WIFI_SSID);
+void saveConfigCallback () {
+    Log.trace("[ %s:%d ] WiFi manager registered changes, should save config", __FILE__, __LINE__);
+    shouldSaveConfig = true;
+}
+
+void configModeCallback (WiFiManager *myWiFiManager) {
+    Log.warning("Entered WiFi config mode with IP %s", WiFi.softAPIP().toString().c_str());
+    Log.warning("SSID '%s'", myWiFiManager->getConfigPortalSSID().c_str());
+  
+    //entered config mode, make led toggle faster
     startBlinkOnboardLed(true);
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);   
 }
 
 void onWifiConnected (const WiFiEventStationModeConnected& event) {
@@ -304,32 +333,150 @@ void onWifiConnected (const WiFiEventStationModeConnected& event) {
 
 void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
 	Log.warning("[ %s:%d ] Disconnected from SSID [ %s ] with reason [ %d ].", __FILE__, __LINE__, event.ssid.c_str(), event.reason);
-    m_wifiReconnectTimer.once(2, connectToWifi);
+    startBlinkOnboardLed(true);
 }
 
 void onWifiGotIP(const WiFiEventStationModeGotIP& event) {
     Log.notice("[ %s:%d ] Received IP [ %s ] / Gateway [ %s ] / Mask [ %s ].", __FILE__, __LINE__, event.ip.toString().c_str(), event.gw.toString().c_str(), event.mask.toString().c_str());
 	stopBlinkOnboardLed();
 
-    if (!MDNS.begin(m_clientId)) {
+    MDNS.close();
+    if (!MDNS.begin(clientId)) {
         Log.error("[ %s:%d ] Error setting up MDNS responder.", __FILE__, __LINE__);
     }
 }
 
-void setupWifi() {
-    WiFi.disconnect() ;
-    WiFi.persistent(false);
-    m_wifiConnectHandler = WiFi.onStationModeConnected(onWifiConnected);
-    m_wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
-    m_wifiGotIpHandler = WiFi.onStationModeGotIP(onWifiGotIP);
+void setupWifiManager() {
+    char charBuffer[1024];
+    WiFiManager wifiManager;
 
-    String mac = WiFi.macAddress();
-    mac.replace(":", "");
-    m_clientId = String(CLIENT_ID_PREFIX) + "-" + mac;
+    //uncomment to reset saved settings and clean file system for debug purpose
+    //wifiManager.resetSettings(); LittleFS.format();
 
-    WiFi.hostname(m_clientId);
+    // start blinking slow because we start in AP mode and try to connect
+    startBlinkOnboardLed();
 
-    connectToWifi();    
+    clientId = String(CLIENT_ID_PREFIX) + String(ESP.getChipId());
+    WiFi.hostname(clientId);
+    WiFi.setAutoReconnect(true);    
+
+    //read configuration from FS json
+    Log.trace("[ %s:%d ] Mounting file system", __FILE__, __LINE__);
+
+    if (LittleFS.begin()) {
+        Log.notice("[ %s:%d ] Successfully mounted file system", __FILE__, __LINE__);
+        if (LittleFS.exists("/config.json")) {
+            //file exists, reading and loading
+            Log.trace("[ %s:%d ] Reading JSON config file", __FILE__, __LINE__);
+            File configFile = LittleFS.open("/config.json", "r");
+            if (configFile) {
+                Log.trace("[ %s:%d ] Successfully opened JSON config file", __FILE__, __LINE__);
+                size_t size = configFile.size();
+                // Allocate a buffer to store contents of the file.
+                std::unique_ptr<char[]> buf(new char[size]);
+
+                configFile.readBytes(buf.get(), size);
+
+                DynamicJsonDocument json(1024);
+                auto deserializeError = deserializeJson(json, buf.get());
+                
+                if ( ! deserializeError ) {
+                    serializeJson(json, charBuffer, sizeof(charBuffer));
+                    Log.trace("[ %s:%d ] Parsed JSON", __FILE__, __LINE__);
+                    Log.trace("[ %s:%d ] %s", __FILE__, __LINE__, charBuffer);
+                    
+                    strcpy(mqttServer, json["mqtt_server"]);
+                    strcpy(mqttPort, json["mqtt_port"]);
+                    strcpy(mqttUser, json["mqtt_user"]);
+                    strcpy(mqttPassword, json["mqtt_password"]);
+                    strcpy(shutterDelay, json["shutter_delay"]);
+                } else {
+                    Log.error("[ %s:%d ] Failed to load JSON config file, consider reset", __FILE__, __LINE__);
+                }
+                
+                configFile.close();
+            }
+        } else {
+            Log.notice("[ %s:%d ] JSON config file does not exist", __FILE__, __LINE__);
+        }
+    } else {
+        Log.fatal("[ %s:%d ] Failed to mount file system", __FILE__, __LINE__);
+    }
+
+    wifiManager.setAPCallback(configModeCallback);
+    wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+    WiFiManagerParameter custom_mqtt_server("server", "MQTT broker", mqttServer, sizeof(mqttServer));
+    wifiManager.addParameter(&custom_mqtt_server);
+
+    WiFiManagerParameter custom_mqtt_port("port", "MQTT port", mqttPort, sizeof(mqttPort));
+    wifiManager.addParameter(&custom_mqtt_port);
+
+    WiFiManagerParameter custom_mqtt_user("user", "MQTT user", mqttUser, sizeof(mqttUser));
+    wifiManager.addParameter(&custom_mqtt_user);
+
+    WiFiManagerParameter custom_mqtt_password("password", "MQTT password", mqttPassword, sizeof(mqttPassword));
+    wifiManager.addParameter(&custom_mqtt_password);
+
+    WiFiManagerParameter custom_shutter_delay("delay", "Shutter delay in MS", shutterDelay, 4);
+    wifiManager.addParameter(&custom_shutter_delay);
+
+    wifiManager.setTimeout(120);
+    if(!wifiManager.autoConnect()) {
+        Log.fatal("[ %s:%d ] Failed to connect to WiFi and reached timeout, restart now...", __FILE__, __LINE__);
+        delay(3000);
+        
+        //reset and try again, or maybe put it to deep sleep
+        ESP.reset();
+        delay(5000);
+    } 
+
+    //if you get here you have connected to the WiFi
+    Log.notice("[ %s:%d ] Connected to SSID [ %s ] on BSSID [ %s ] via channel [ %d ], waiting for IP address.", __FILE__, __LINE__, WiFi.SSID().c_str(), WiFi.BSSIDstr().c_str(), WiFi.channel());
+
+    //read updated parameters
+    strcpy(mqttServer, custom_mqtt_server.getValue());
+    strcpy(mqttPort, custom_mqtt_port.getValue());
+    strcpy(mqttUser, custom_mqtt_user.getValue());
+    strcpy(mqttPassword, custom_mqtt_password.getValue());
+    strcpy(shutterDelay, custom_shutter_delay.getValue());
+
+    Log.notice("[ %s:%d ] MQTT broker settings [ %s:%s ]", __FILE__, __LINE__, mqttServer, mqttPort);
+    Log.notice("[ %s:%d ] MQTT user [ %s ] and password  [ %s ]", __FILE__, __LINE__, mqttUser, mqttPassword);
+    Log.notice("[ %s:%d ] Shutter delay [ %s ] ms", __FILE__, __LINE__, shutterDelay);
+
+    //save the custom parameters to file system
+    if (shouldSaveConfig) {
+        Log.notice("[ %s:%d ] Saving JSON config", __FILE__, __LINE__);
+        DynamicJsonDocument json(1024);
+        json["mqtt_server"] = mqttServer;
+        json["mqtt_port"] = mqttPort;
+        json["mqtt_user"] = mqttUser;
+        json["mqtt_password"] = mqttPassword;
+        json["shutter_delay"] = shutterDelay;
+
+        File configFile = LittleFS.open("/config.json", "w");
+        if (!configFile) {
+            Log.error("[ %s:%d ] Failed to open JSON config file for writing", __FILE__, __LINE__);
+        }
+
+        serializeJson(json, configFile);
+        configFile.close();
+
+        serializeJson(json, charBuffer, sizeof(charBuffer));
+        Log.notice("[ %s:%d ] Successfully wrote config JSON", __FILE__, __LINE__);
+        Log.notice(charBuffer);
+    }
+
+    stopBlinkOnboardLed();
+
+    wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+    wifiConnectHandler = WiFi.onStationModeConnected(onWifiConnected);
+    wifiGotIpHandler = WiFi.onStationModeGotIP(onWifiGotIP);
+
+    if (!MDNS.begin(clientId)) {
+        Log.error("[ %s:%d ] Error setting up MDNS responder", __FILE__, __LINE__);
+    }
 }
 
 void setup() {
@@ -343,13 +490,14 @@ void setup() {
 
     pinMode(LED_BUILTIN, OUTPUT);
 
+    setupWifiManager();
     setupShutter();
     setupMqtt();    
-    setupWifi();
 }
 
 void loop() {
+    MDNS.update();
     checkMqttConnection();
-    m_shutter1.tick();
-    m_shutter2.tick();
+    shutter1.tick();
+    shutter2.tick();
 }
